95e25ee181c75a2c3389ed0ea28d091c5f35d9a4
rs691 <robstewart@my365.bellvue.edu>
2024-10-03 00:50:25 -0500

    add deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 
-
-
-// import ParallaxPage from './components/ParallaxPage'
-// import { CustomButtons } from './components/CustomButtons.jsx'
 import './App.css'
-// import CardParallaxLayout from './components/CardParallaxLayout'
+
 import TextOnlyParallax from './components/TextOnlyParallax'
 
 function App() {
   return (
     <>
-      {/* <ParallaxPage/> */}
-      {/* <CardParallaxLayout/> */}
-      
-      {/* <CustomButtons/> */}
-      {/* <TextOnlyParallax/> */}
+  
       <TextOnlyParallax/>
     </>
   )
