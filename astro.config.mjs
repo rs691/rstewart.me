139dacf7ee66b54c37b3520aa3cb877d403b9c14
rs691 <robstewart@my365.bellevue.edu>
2025-05-06 15:06:51 -0500

    updated config and file names 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-    devToolbar: { enabled: false },
-    site: 'https://rs691/rstewart.me/',
-    base: '/rstewart.me',
-    integrations: [mdx(), sitemap(), react()],
+  devToolbar: { enabled: false },
+  site: 'https://rstewart.me', // Correct format for your domain
+  base: '/',                  // Remove or set to '/' for a root domain
+  integrations: [mdx(), sitemap(), react()],
 });
