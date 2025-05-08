f78f84bce876dc0c59bd84702678c65f3a13ccf2
rs691 <robstewart@my365.bellevue.edu>
2025-05-08 12:56:22 -0500

    fixed paths 18

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,6 @@ import react from '@astrojs/react';
 export default defineConfig({
   devToolbar: { enabled: false },
   site: 'https://rstewart.me', // Correct format for your domain
-  base: '/rstewart.me',                  // Remove or set to '/' for a root domain
+  base: '/rs691/rstewart.me/',                  // Remove or set to '/' for a root domain
   integrations: [mdx(), sitemap(), react()],
 });
