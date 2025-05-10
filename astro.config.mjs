d988073d93ab8a4882628bd0ce35c1ecaf4dc7b9
rs691 <robstewart@my365.bellevue.edu>
2025-05-10 10:25:33 -0500

    added rest of files 6

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,8 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   devToolbar: { enabled: false },
-  site: 'https://rstewart.me/', // Correct format for your domain        
-  base: '/rstewart.me', // Remove or set to '/' for a root domain
+  site: 'https://rstewart.me/',
+  base: '/rstewart.me',
+  trailingSlash: 'always',
   integrations: [mdx(), sitemap(), react()],
 });
